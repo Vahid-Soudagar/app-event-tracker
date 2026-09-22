aaf447d5745ccf15db53c4e27b31a034d1cff7e6
Vahid_Soudagar <mohammadvahidsoudagar264@gmail.com>
2026-09-22 12:24:33 +0530

    add in-memory deduplication logic

--- sdk/src/main/java/com/example/appeventtracker/sdk/Analytics.kt
+++ sdk/src/main/java/com/example/appeventtracker/sdk/Analytics.kt
@@ -8,13 +8,16 @@ import kotlinx.serialization.json.Json
 
 object Analytics {
 
+    private val processedInstallations = mutableSetOf<String>()
+    private val processedVisitSessions = mutableSetOf<String>()
+
     fun reportEvents(json: String, context: Context) {
         Log.d("DebugTag", "Json is $json")
         val request = Json.decodeFromString<EventRequest>(json)
         Log.d("DebugTag", "Request $request")
         val sessionId = SessionManager.getSessionId()
         val installationId = InstallationManager.getInstallationId(context = context)
-        val events = request.events.map { eventType ->
+        val enrichedEvents = request.events.map { eventType ->
             Event(
                 eventType = eventType,
                 timeStamp = System.currentTimeMillis(),
@@ -22,6 +25,34 @@ object Analytics {
                 installationId = installationId
             )
         }
-        Log.d("DebugTag", "Events $events")
+        Log.d("DebugTag", "Events $enrichedEvents")
+
+        val validEvents = enrichedEvents.filter { event ->
+            when (event.eventType) {
+                "INSTALL" -> {
+                    if (processedInstallations.contains(event.installationId)) {
+                        false
+                    } else {
+                        processedInstallations.add(event.installationId)
+                        true
+                    }
+                }
+
+                "VISIT" -> {
+                    if (processedVisitSessions.contains(event.sessionId)) {
+                        false
+                    } else {
+                        processedVisitSessions.add(event.sessionId)
+                        true
+                    }
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+
+        Log.d("DebugTag", "Valid Events $validEvents")
     }
 }
